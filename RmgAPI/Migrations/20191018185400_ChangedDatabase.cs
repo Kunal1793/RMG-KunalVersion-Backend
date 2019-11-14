9f048812a24e59af9250ab7db58d2cad42215cde
Administrator <kkulka01@nyit.edu>
2019-11-13 20:34:02 -0500

    Kunal Version of Final RMG Project

---
+++ RmgAPI/Migrations/20191018185400_ChangedDatabase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RmgAPI.Migrations
+{
+    public partial class ChangedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ListOfRequests_Accounts_AccountID",
+                table: "ListOfRequests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ListOfRequests_AccountID",
+                table: "ListOfRequests");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ListOfRequests_AccountID",
+                table: "ListOfRequests",
+                column: "AccountID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ListOfRequests_Accounts_AccountID",
+                table: "ListOfRequests",
+                column: "AccountID",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ RmgAPI/Models/RMGdbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RmgAPI.Models
+{
+    public class RMGdbContext: DbContext
+    {
+        public RMGdbContext(DbContextOptions<RMGdbContext> options) : base(options)
+        { }
+
+        public DbSet<Account> accounts { get; set; }
+        public DbSet<ListOfRequests> listOfRequests { get; set; }
+    }
+}
